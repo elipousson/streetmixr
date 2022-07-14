21690d15bba8f5b4e5f2759fb8d9b0de3e40de0d
Eli Pousson <eli.pousson@gmail.com>
2022-07-14 14:30:35 -0400

    test: add tests for get_street

---
+++ tests/testthat/test-get_street.R
@@ -0,0 +1,17 @@
+test_that("get_street works", {
+  test_url <- "https://streetmix.net/eli.pousson/7/n-conkling-street-north-of-eastern-avenue"
+  test_user_id <- "eli.pousson"
+  test_street_id <- "b26529c0-c1ad-11ec-877c-abfad556cfc6"
+  test_count <- 2
+
+  expect_type(get_street(url = test_url), "list")
+  expect_type(get_street(url = test_url, return = "data"), "list")
+  expect_type(get_street(url = test_url, return = "street"), "list")
+  expect_type(get_street(url = test_url, return = "location"), "list")
+  expect_s3_class(get_street(url = test_url, return = "segments"), "data.frame")
+  expect_type(get_street(user_id = test_user_id), "list")
+  expect_type(get_street(street_id = test_street_id), "list")
+  expect_type(get_street(count = test_count), "list")
+})
+
+
